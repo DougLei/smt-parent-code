81626984fd7d83c06561b3e9b0e97d69eb4ce0ec
DougLei <953034206@qq.com>
2021-05-15 14:20:12 +0800

    t

--- src/main/java/com/smt/parent/code/filters/token/TokenEntity.java
+++ src/main/java/com/smt/parent/code/filters/token/TokenEntity.java
@@ -1,14 +1,14 @@
 package com.smt.parent.code.filters.token;
 
 import java.util.Date;
+import java.util.UUID;
 
 /**
  * 
  * @author DougLei
  */
 public class TokenEntity {
-	private Date currentDate = new Date();
-	private String token;// token
+	private String value;// token值
 	private String accountId;// 账户id
 	private String userId; // 用户id
 	private String projectCode;// 项目code
@@ -17,60 +17,81 @@ public class TokenEntity {
 	private String clientIp;// 登陆的客户端ip
 	private Date lastOpDate;// 最后操作时间
 	private int loginFailTimes;// (连续)登录失败的次数
+	private Date currentDate; // 当前时间
+	private String uuid; // uuid
 	
-	public Date getCurrentDate() {
-		return currentDate;
-	}
-	public String getToken() {
-		return token;
-	}
-	public void setToken(String token) {
-		this.token = token;
+	public String getValue() {
+		return value;
 	}
 	public String getAccountId() {
 		return accountId;
 	}
-	public void setAccountId(String accountId) {
-		this.accountId = accountId;
-	}
 	public String getUserId() {
 		return userId;
-	}
-	public void setUserId(String userId) {
-		this.userId = userId;
 	}
 	public String getProjectCode() {
 		return projectCode;
 	}
-	public void setProjectCode(String projectCode) {
-		this.projectCode = projectCode;
-	}
 	public String getTenantId() {
 		return tenantId;
-	}
-	public void setTenantId(String tenantId) {
-		this.tenantId = tenantId;
 	}
 	public Date getLoginDate() {
 		return loginDate;
 	}
-	public void setLoginDate(Date loginDate) {
-		this.loginDate = loginDate;
-	}
 	public String getClientIp() {
 		return clientIp;
-	}
-	public void setClientIp(String clientIp) {
-		this.clientIp = clientIp;
 	}
 	public Date getLastOpDate() {
 		return lastOpDate;
 	}
-	public void setLastOpDate(Date lastOpDate) {
-		this.lastOpDate = lastOpDate;
-	}
 	public int getLoginFailTimes() {
 		return loginFailTimes;
+	}
+	public Date getCurrentDate() {
+		if(currentDate == null)
+			currentDate = new Date();
+		return currentDate;
+	}
+	public Date getCurrentDate(boolean cache) { // cache是否使用缓存数据
+		if(cache)
+			return getCurrentDate();
+		return new Date();
+	}
+	public String getUUID() {
+		if(uuid == null)
+			uuid = UUID.randomUUID().toString();
+		return uuid;
+	}
+	public String getUUID(boolean cache) {  // cache是否使用缓存数据
+		if(cache)
+			return getUUID();
+		return UUID.randomUUID().toString();
+	}
+	
+	// -------------------------------------------------------------------------------------------------
+	public void setValue(String value) {
+		this.value = value;
+	}
+	public void setAccountId(String accountId) {
+		this.accountId = accountId;
+	}
+	public void setUserId(String userId) {
+		this.userId = userId;
+	}
+	public void setProjectCode(String projectCode) {
+		this.projectCode = projectCode;
+	}
+	public void setTenantId(String tenantId) {
+		this.tenantId = tenantId;
+	}
+	public void setLoginDate(Date loginDate) {
+		this.loginDate = loginDate;
+	}
+	public void setClientIp(String clientIp) {
+		this.clientIp = clientIp;
+	}
+	public void setLastOpDate(Date lastOpDate) {
+		this.lastOpDate = lastOpDate;
 	}
 	public void setLoginFailTimes(int loginFailTimes) {
 		this.loginFailTimes = loginFailTimes;
