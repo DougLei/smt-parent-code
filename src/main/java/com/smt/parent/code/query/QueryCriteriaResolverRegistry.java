59e2e3df888e7fd8374aa5b433bb857247a1bd84
DougLei <953034206@qq.com>
2021-06-02 18:24:49 +0800

    T

--- src/main/java/com/smt/parent/code/query/QueryCriteriaResolverRegistry.java
+++ src/main/java/com/smt/parent/code/query/QueryCriteriaResolverRegistry.java
@@ -2,6 +2,7 @@ package com.smt.parent.code.query;
 
 import java.util.List;
 
+import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.web.method.support.HandlerMethodArgumentResolver;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
@@ -13,8 +14,13 @@ import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 @Configuration
 public class QueryCriteriaResolverRegistry implements WebMvcConfigurer{
 	
+	@Bean
+	public QueryCriteriaResolver queryCriteriaResolver() {
+		return new QueryCriteriaResolver();
+	}
+	
 	@Override
 	public void addArgumentResolvers(List<HandlerMethodArgumentResolver> resolvers) { 
-		resolvers.add(new QueryCriteriaResolver()); // 添加自定义的方法参数解析器
+		resolvers.add(queryCriteriaResolver()); // 添加自定义的方法参数解析器
 	}
 }
